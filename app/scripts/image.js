724de8a73c39fad4f18f8951f03ece6d710a40be
gongbaodd <gongbaodd@gmail.com>
2015-05-20 10:05:56 +0800

    panorama

---
+++ app/scripts/image.js
@@ -0,0 +1,51 @@
+'use strict';
+(function(w){
+    var THREE = w.THREE;
+    
+    function Image3D(scene,camera,texture,args){
+        THREE.Object3D.call(this);
+        
+        var geometry = new THREE.PlaneBufferGeometry(texture.image.width/args.scale||100,texture.image.height/args.scale||100);
+        var material = new THREE.MeshBasicMaterial({
+            map:texture,
+            transparent:true,
+            opacity:1.0
+        });
+        var mesh = new THREE.Mesh(geometry,material);
+        
+        this.mesh = mesh;
+        this.camera = camera;
+        this.add(mesh);
+        this.args = args;
+        this.degree = args.degree;
+        this.updatePosition();
+        this.intersectable = this.args.onFocus!==undefined||this.args.onBlur!==undefined;
+        
+        scene.add(this);
+        
+        if(this.intersectable){
+            scene.intersectables.push(this.children[0]);
+        }
+        this.remove = function(){
+            if(this.intersectable){
+                scene.iintersectables.splice($.inArray(this.children[0],scene.intersectables),1);
+            }
+            scene.remove(this);
+        };
+    }
+    Image3D.prototype = Object.create(THREE.Object3D.prototype);
+    Image3D.prototype.constructor = Image3D;
+    
+    Image3D.prototype.updatePosition = function(){
+        var phi = (this.args.verticalDegree)*Math.PI/180;
+		var theta = (this.args.degree-180)*Math.PI/180;
+		
+		this.position.x = -this.args.radius * Math.cos(phi) * Math.cos(theta);
+		this.position.y = this.args.radius * Math.sin(phi);
+		this.position.z = this.args.radius * Math.cos(phi) * Math.sin(theta);
+		
+		this.lookAt(this.camera.position);
+    };
+    
+    w.Image3D = Image3D;
+})(window);
---
+++ app/scripts/panorama.js
@@ -0,0 +1,30 @@
+'use strict';
+(function(w){
+    var THREE = w.THREE;
+    
+    function Panorama(scene,textureUrl,args) {
+        args = {} || args;
+        var sphere = new THREE.Mesh(
+            new THREE.SphereGeometry(args.depth||100,32,32),
+            new THREE.MeshBasicMaterial({
+                map:THREE.ImageUtils.loadTexture(textureUrl),
+                transparent:args.transparent || false,
+                opacity:0.5
+            })
+        );
+        
+        sphere.scale.x = -1;
+        
+        this.sphere = sphere;
+        
+        scene.add(sphere);
+        
+        this.remove = function(){
+            scene.remove(sphere);
+        };
+    }
+    Panorama.prototype = Object.create(THREE.Object3D.prototype);
+    Panorama.prototype.constructor = Panorama;
+    
+    w.Panorama = Panorama;
+})(window);
