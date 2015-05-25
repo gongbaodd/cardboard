5f153b3e3c644ef37f79d3fd132d63df84263f64
gongbaodd <gongbaodd@gmail.com>
2015-05-25 16:17:50 +0800

    启用gzip

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 var serve = require('koa-static');
+var compress = require('koa-compress');
 var koa = require('koa');
 var app = koa();
 
+app.use(compress());
 app.use(serve('../dist'));
 
 app.listen(3000);
